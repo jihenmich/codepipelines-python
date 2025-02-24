dcbd8fde79a45e17c36bb0253221ef4070e4e49e
jihenmich <jihen.michalski@outlook.com>
2025-02-24 13:31:04 +0100

    Create test_name_alter.py

---
+++ test_name_alter.py
@@ -0,0 +1,16 @@
+import pytest
+from io import StringIO
+import sys
+
+
+def test_ist_gerade():
+    assert ist_gerade(42) == True
+    assert ist_gerade(7) == False
+
+
+def test_hallo_message(capsys):
+    name = "Jihen"
+    alter = 28
+    print(f"Hallo {name}, in 10 Jahren wirst du {alter + 10} Jahre alt sein!")
+    captured = capsys.readouterr()
+    assert captured.out == "Hallo Jihen, in 10 Jahren wirst du 38 Jahre alt sein!\n"
